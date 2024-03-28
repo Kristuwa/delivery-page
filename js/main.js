7f42f150d26132a208ac8665f116e738440984f4
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2024-03-29 00:36:01 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const slider = document.querySelector(".questions__list");
-  const items = document.querySelectorAll(".questions__item");
-  const btn = document.querySelector(".btn");
-  const itemWidth = items[0].offsetWidth;
-  let currentIndex = 0;
+  const slider = document.querySelector(".slider");
+  const button = slider.querySelector("button");
+  const list = slider.querySelector(".questions__list");
+  const items = slider.querySelectorAll(".questions__item");
 
-  btn.addEventListener("click", function () {
-    currentIndex++;
-    if (currentIndex > items.length) {
-      currentIndex = 0;
-      slider.style.transition = "none"; // Отключаем анимацию для плавного перехода к началу
-      slider.style.transform = `translateX(0px)`; // Перемещаемся к началу списка без анимации
-      setTimeout(() => {
-        slider.style.transition = ""; // Возвращаем анимацию после перехода к началу списка
-      }, 50);
+  // Получаем ширину одного элемента вопроса в vw
+  const itemWidth = 0.93; // Ширина между элементами в vw
+
+  let currentPosition = 0;
+
+  button.addEventListener("click", function () {
+    // Добавляем ширину элемента вопроса к расстоянию между элементами при смещении
+    currentPosition -=
+      ((itemWidth + 1) * items[0].offsetWidth) /
+      document.documentElement.clientWidth;
+
+    // Устанавливаем границу, при достижении которой мы возвращаемся в начало списка
+    if (currentPosition < -itemWidth * 3) {
+      currentPosition = 0;
     }
-    updateSlider();
-  });
 
-  function updateSlider() {
-    const offset = -currentIndex * itemWidth;
-    slider.style.transform = `translateX(${offset}px)`;
-  }
+    items.forEach((item) => {
+      // Смещаем каждый элемент на расстояние itemWidth плюс ширина элемента вопроса
+      item.style.transform = `translateX(${currentPosition * 100}vw)`;
+    });
+  });
 });
